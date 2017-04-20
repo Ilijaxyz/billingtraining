c6ebe951624979e99320f8a21045c9743d19c63c
Ilijaxyz <cuqale@gmail.com>
2017-04-21 01:50:01 +0200

    new update

---
+++ Billing.Api/Billing.Api/Models/ReportModels/SalesByAgentModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Billing.Api.Models
+{
+    public class SalesByAgentModel
+    {
+        public string AgentName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public double AgentTotal { get; set; }
+        public double ProcentTotal { get; set; }
+        public List<RegionAgentSales> Sales { get; set; }
+    }
+    public class RegionAgentSales
+    {
+        public string RegionName { get; set; }
+
+        public double RegionTotal { get; set; }
+
+        public double RegionPercent { get; set; }
+
+        public double TotalPercent { get; set; }
+    }
+}
---
+++ Billing.Api/Billing.Api/Reports/SetOfReports.cs
@@ -0,0 +1,38 @@
+﻿using Billing.Api.Helpers;
+using Billing.Api.Models;
+using Billing.Repository;
+using System;
+using System.Web.Http;
+
+namespace Billing.Api.Reports
+{
+    public class SetOfReports
+    {
+        private UnitOfWork _unitOfWork;
+        public SetOfReports(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        private DashboardReport _dashboard;
+        private SalesByRegionReport _salesByRegion;
+        private SalesByCustomerReport _salesByCustomer;
+        private SalesByCategoryReport _salesByCategory;
+        private AgentsByRegionReport _agentsByRegion;
+        private CustomersByCategoryReport _customersByCategory;
+        private InvoicesReviewReport _invoiceReview;
+        private InvoiceReport _invoice;
+        private StockLevelReport _stocklevel;
+
+        public DashboardReport Dashboard { get { return _dashboard ?? (_dashboard = new DashboardReport(_unitOfWork)); } }
+        public SalesByRegionReport SalesByRegion { get { return _salesByRegion ?? (_salesByRegion = new SalesByRegionReport(_unitOfWork)); } }
+        public SalesByCustomerReport SalesByCustomer { get { return _salesByCustomer ?? (_salesByCustomer = new SalesByCustomerReport(_unitOfWork)); } }
+        public SalesByCategoryReport SalesByCategory { get { return _salesByCategory ?? (_salesByCategory = new SalesByCategoryReport(_unitOfWork)); } }
+        public AgentsByRegionReport AgentsByRegion { get { return _agentsByRegion ?? (_agentsByRegion = new AgentsByRegionReport(_unitOfWork)); } }
+        public CustomersByCategoryReport CustomersByCategory { get { return _customersByCategory ?? (_customersByCategory = new CustomersByCategoryReport(_unitOfWork)); } }
+        public InvoicesReviewReport InvoiceReview { get { return _invoiceReview ?? (_invoiceReview = new InvoicesReviewReport(_unitOfWork)); } }
+        public InvoiceReport Invoice { get { return _invoice ?? (_invoice = new InvoiceReport(_unitOfWork)); } }
+        public StockLevelReport StockLevel { get { return _stocklevel ?? (_stocklevel = new StockLevelReport(_unitOfWork)); } }
+    }
+}
+
